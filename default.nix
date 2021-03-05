27e214fb8ff6200c0dd488bc41924e3df62eaab7
Daniil Iaitskov <daniil.iaitskov@soostone.com>
2021-03-05 10:07:08 -0500

    master hello overlay

--- default.nix
+++ default.nix
@@ -1,4 +1,16 @@
 let
   sources = import ./nix/sources.nix;
+
+  hello = pkgs.writeShellScriptBin "hello" ''
+    echo "Hello from the Nix channel overlay!"
+  '';
+
+  pkgs = import sources.nixpkgs {
+    overlays = [
+      (self: super: {
+        inherit hello;
+      })
+    ];
+  };
 in
-  sources.nixpkgs
+  pkgs
